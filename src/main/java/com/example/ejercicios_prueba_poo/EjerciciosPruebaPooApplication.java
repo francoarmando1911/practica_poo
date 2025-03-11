f5acfce85abcba4d37cfece88d42d9c8251ae380
Franco Armando <francoarmando1911@gmail.com>
2025-03-11 20:13:12 -0300

    Subida inicial de todos los archivos

---
+++ src/main/java/com/example/ejercicios_prueba_poo/EjerciciosPruebaPooApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicios_prueba_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosPruebaPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosPruebaPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejercicios_prueba_poo/EjerciciosPruebaPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicios_prueba_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosPruebaPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
