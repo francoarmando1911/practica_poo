a62834decd0a87fdd797062d87222dcbbfe2395c
Franco Armando <francoarmando1911@gmail.com>
2025-03-13 18:09:01 -0300

    Ejercicio de generar contraseñas - en este ejercicio se emplean metodos para definir contraseñas con caracteres aleatorios (mayusuclas, minusculas y numeros), asi como un metodo que recorre la contraseña y devuelbe en valor booleano si esta es fuerte o no. En el metodo main se definen los arrays que contienen las contraseñas y se pide al usuario que ingrese el numero de pass que desea generar y de cuantos caracteres desea que sean. Al final se recorren en un for, se almacenan en el array y se muestran los resultados con las contraseñas y si estas generadas osn fuertes o no, con el metodo que mencione anteriormente

--- src/main/java/Ejercicios/Password.java
+++ src/main/java/Ejercicios/Password.java
@@ -1,5 +1,8 @@
 package Ejercicios;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /*
 Haz una clase llamada Password que siga las siguientes condiciones:
 
@@ -23,4 +26,100 @@ Ahora, crea una clase clase ejecutable:
 - Al final, muestra la contraseña y si es o no fuerte (usa el bucle anterior). Usa este simple formato: contraseña1 valor_booleano1
  */
 public class Password {
+
+    private int longitud;
+    private String contrasena;
+
+    public Password() {
+        this.longitud = 8;
+        this.contrasena = generarContrasena(this.longitud);
+    }
+
+    public Password(int longitud) {
+        this.longitud = longitud;
+        this.contrasena = generarContrasena(this.longitud);
+    }
+
+    //Metodo para generar una contraseña aleatoria
+    private String generarContrasena(int longitud){
+        String mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String minusculas = "abcdefghijklmnopqrstuvwxyz";
+        String numeros = "0123456789";
+        String caracteres = mayusculas + minusculas + numeros;
+
+        StringBuilder contrasena = new StringBuilder();
+        Random random = new Random();
+
+        for(int i = 0; i < longitud; i++){
+            //Selecciono un indice aleatorio
+            int indice = random.nextInt(caracteres.length());
+            //Agrego el caracter a la contraseña
+            contrasena.append(caracteres.charAt(indice));
+        }
+
+        return contrasena.toString();
+    }
+
+    public boolean esFuerte(){
+        int mayusculas = 0;
+        int minusculas = 0;
+        int numeros = 0;
+
+        //Rrecorrer la contraseña caracter por caracter
+
+        for(int i = 0; i < contrasena.length(); i++){
+            char caracter = contrasena.charAt(i);
+
+            if(Character.isUpperCase(caracter)){
+                mayusculas++;
+            } else if (Character.isLowerCase(caracter)) {
+                minusculas++;
+            } else if (Character.isDigit(caracter)) {
+                numeros++;
+            }
+        }
+
+        //Evaluar si cumple con los requisitros para ser una contraseña fuerte
+        return (mayusculas > 2 && minusculas > 1 && numeros > 5);
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
+
+class Main2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Pedir el tamaño del array
+        System.out.print("Ingrese el número de contraseñas a generar: ");
+        int cantidad = scanner.nextInt();
+
+        Password[] passwords = new Password[cantidad];
+        boolean[] esFuerteArray = new boolean[cantidad];
+
+        System.out.print("Ingrese la longitud de las contraseñas: ");
+        int longitud = scanner.nextInt();
+
+        for (int i = 0; i < cantidad; i++) {
+            passwords[i] = new Password(longitud);
+            esFuerteArray[i] = passwords[i].esFuerte();
+        }
+
+        System.out.println("\nResultados:");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println(passwords[i].getContrasena() + " " + esFuerteArray[i]);
+        }
+        scanner.close();
+    }
 }
