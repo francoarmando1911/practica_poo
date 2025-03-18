4afa08d4638bdf17837d4f3db4a5236fd670fe06
Franco Armando <francoarmando1911@gmail.com>
2025-03-18 20:44:18 -0300

    Ejercicio de cuenta bancaria - inicializacion de nuevo ejercicio de cuenta bancaria, definicion de atributos, constructores, getters y setters y metodo para visualizar los datos. Falta culminar

--- src/main/java/Ejercicios/Cuentabancaria2.java
+++ src/main/java/Ejercicios/Cuentabancaria2.java
@@ -1,5 +1,7 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 /**
  * Se requiere un programa que modele una cuenta bancaria que posee los siguientes atributos:
  * - Nombres del titular.
@@ -22,5 +24,97 @@ package Ejercicios;
 
 public class Cuentabancaria2 {
 
-    
+    String nombre_titular;
+    String apellido_titular;
+    Double numero_cuentaBancaria;
+    String tipo_cuentaBancaria; //Puede ser cuenta ahorro o cuenta corriente
+    Double saldo_cuentaBancaria = 0.0;
+
+    public Cuentabancaria2(String nombre_titular, String apellido_titular, Double numero_cuentaBancaria, String tipo_cuentaBancaria, Double saldo_cuentaBancaria) {
+        this.nombre_titular = nombre_titular;
+        this.apellido_titular = apellido_titular;
+        this.numero_cuentaBancaria = numero_cuentaBancaria;
+        this.tipo_cuentaBancaria = tipo_cuentaBancaria;
+        this.saldo_cuentaBancaria = saldo_cuentaBancaria;
+    }
+
+    public String getNombre_titular() {
+        return nombre_titular;
+    }
+
+    public void setNombre_titular(String nombre_titular) {
+        this.nombre_titular = nombre_titular;
+    }
+
+    public String getApellido_titular() {
+        return apellido_titular;
+    }
+
+    public void setApellido_titular(String apellido_titular) {
+        this.apellido_titular = apellido_titular;
+    }
+
+    public String getTipo_cuentaBancaria() {
+        return tipo_cuentaBancaria;
+    }
+
+    public void setTipo_cuentaBancaria(String tipo_cuentaBancaria) {
+        this.tipo_cuentaBancaria = tipo_cuentaBancaria;
+    }
+
+    public Double getSaldo_cuentaBancaria() {
+        return saldo_cuentaBancaria;
+    }
+
+    public void setSaldo_cuentaBancaria(Double saldo_cuentaBancaria) {
+        this.saldo_cuentaBancaria = saldo_cuentaBancaria;
+    }
+
+    public Double getNumero_cuentaBancaria() {
+        return numero_cuentaBancaria;
+    }
+
+    public void setNumero_cuentaBancaria(Double numero_cuentaBancaria) {
+        this.numero_cuentaBancaria = numero_cuentaBancaria;
+    }
+
+    //Metodo para mostrar inforrmacion
+    public void mostrarInformacion(){
+        System.out.println("Nombre del titular: " + nombre_titular);
+        System.out.println("Apellido del titular: " + apellido_titular);
+        System.out.println("Numero de cuenta bancaria: " + numero_cuentaBancaria);
+        System.out.println("Tipo de cuenta bancaria: " + tipo_cuentaBancaria);
+        System.out.println("Saldo del cuenta: " + saldo_cuentaBancaria);
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del titular: ");
+        String nombre_titular = entrada.nextLine();
+        System.out.println("Ingrese el apellido del titular: ");
+        String apellido_titular = entrada.nextLine();
+        System.out.println("Ingrese el numero de cuenta bancaria: ");
+        Double numero_cuentaBancaria = entrada.nextDouble();
+        System.out.println("Ingrese el tipo de cuenta bancaria (ahorro - corriente): ");
+        String tipo_cuentaBancaria = entrada.nextLine();
+        System.out.println("Ingrese el saldo del cuenta: ");
+        Double saldo_cuentaBancaria = entrada.nextDouble();
+
+        System.out.println("Desea consultar el saldo de su cuenta bancaria actualizado? ");
+        String consulta_saldo;
+
+        if(tipo_cuentaBancaria.equals("ahorro")) {
+            System.out.println("El tipo de cuenta bancaria es ahorro");
+        } else if (tipo_cuentaBancaria.equals("corriente")) {
+            System.out.println("El tipo de cuenta bancaria es corriente");
+        } else {
+            System.out.println("Valor de tipo de cueunta incorrecto");
+        }
+
+    }
+
+
+
+
 }
