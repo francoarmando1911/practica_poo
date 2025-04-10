eed237d503bd5d90e93f2083e6cdde934905b965
Franco Armando <francoarmando1911@gmail.com>
2025-04-10 19:51:35 -0300

    Inicializacion ejercicio medicamento

---
+++ src/main/java/EjercicioMedicamento/MainMedicamento.java
@@ -0,0 +1,13 @@
+package EjercicioMedicamento;
+
+/*
+En un método main se debe crear el medicamento. También se debe
+crear una posología y asignársela al medicamento creado. Se deben mostrar
+en pantalla los datos del medicamento y de su posología.
+ */
+
+public class MainMedicamento {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/EjercicioMedicamento/Medicamento.java
@@ -0,0 +1,17 @@
+package EjercicioMedicamento;
+
+/*
+Se requiere desarrollar un programa que modele la siguiente información
+de un medicamento:
+u Nombre del medicamento.
+u Fabricante del medicamento.
+u Vía de administración del medicamento.
+La clase medicamento debe tener una clase interna, que represente la
+posología del medicamento con la siguiente información:
+u Usuarios del medicamento.
+u Dosis del medicamento en miligramos
+u Periodo de tiempo para tomar el medicamento.
+u Recomendaciones.
+ */
+public class Medicamento {
+}
