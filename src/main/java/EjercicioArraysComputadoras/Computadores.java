8742a886091459db75c97dfc5be5387b7661e963
Franco Armando <francoarmando1911@gmail.com>
2025-04-10 20:20:54 -0300

    Inicializacion ejercicio arrays computadores

---
+++ src/main/java/EjercicioArraysComputadoras/Computadores.java
@@ -0,0 +1,4 @@
+package EjercicioArraysComputadoras;
+
+public class Computadores {
+}
---
+++ src/main/java/EjercicioArraysComputadoras/MainTienda.java
@@ -0,0 +1,29 @@
+package EjercicioArraysComputadoras;
+
+/*
+
+Se requiere desarrollar un programa que modele una tienda de computa-
+dores. La tienda posee los siguientes atributos:
+u Nombre de la tienda.
+u Propietario de la tienda.
+u Identificador tributario de la tienda.
+
+Los computadores de la tienda tienen los siguientes atributos:
+u Marca del computador.
+u Cantidad de memoria.
+u Características del procesador.
+u Sistema operativo.
+u Precio del computador.
+
+El programa debe poseer métodos que permitan:
+u Agregar un computador a la tienda.
+u Eliminar un computador de la tienda dada su marca.
+u Buscar un computador en la tienda dada su marca.
+u Listar la información de todos los computadores que tiene la tienda.
+
+ */
+public class MainTienda {
+    public static void main(String[] args) {
+
+    }
+}
