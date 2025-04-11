a9507f5d00e29c4d055b666cb4a355efad494989
Franco Armando <francoarmando1911@gmail.com>
2025-04-11 07:50:48 -0300

    actualizacion

--- src/main/java/EjercicioArraysComputadoras/Computadores.java
+++ src/main/java/EjercicioArraysComputadoras/Computadores.java
@@ -1,4 +1,13 @@
 package EjercicioArraysComputadoras;
 
+/*
+Los computadores de la tienda tienen los siguientes atributos:
+u Marca del computador.
+u Cantidad de memoria.
+u Características del procesador.
+u Sistema operativo.
+u Precio del computador.
+ */
+
 public class Computadores {
 }
--- src/main/java/EjercicioArraysComputadoras/MainTienda.java
+++ src/main/java/EjercicioArraysComputadoras/MainTienda.java
@@ -1,13 +1,6 @@
 package EjercicioArraysComputadoras;
 
 /*
-Los computadores de la tienda tienen los siguientes atributos:
-u Marca del computador.
-u Cantidad de memoria.
-u Características del procesador.
-u Sistema operativo.
-u Precio del computador.
-
 El programa debe poseer métodos que permitan:
 u Agregar un computador a la tienda.
 u Eliminar un computador de la tienda dada su marca.
