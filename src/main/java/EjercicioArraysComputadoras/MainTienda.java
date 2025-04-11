b48f1762119d2aa205d07acce46da95b28326764
Franco Armando <francoarmando1911@gmail.com>
2025-04-11 07:49:57 -0300

    Clase TiendaComputaodres creacion de constructores y getters y setters

--- src/main/java/EjercicioArraysComputadoras/MainTienda.java
+++ src/main/java/EjercicioArraysComputadoras/MainTienda.java
@@ -1,13 +1,6 @@
 package EjercicioArraysComputadoras;
 
 /*
-
-Se requiere desarrollar un programa que modele una tienda de computa-
-dores. La tienda posee los siguientes atributos:
-u Nombre de la tienda.
-u Propietario de la tienda.
-u Identificador tributario de la tienda.
-
 Los computadores de la tienda tienen los siguientes atributos:
 u Marca del computador.
 u Cantidad de memoria.
--- src/main/java/EjercicioArraysComputadoras/TiendaComputadores.java
+++ src/main/java/EjercicioArraysComputadoras/TiendaComputadores.java
@@ -1,4 +1,45 @@
 package EjercicioArraysComputadoras;
 
+/*
+Se requiere desarrollar un programa que modele una tienda de computa-
+dores. La tienda posee los siguientes atributos:
+u Nombre de la tienda.
+u Propietario de la tienda.
+u Identificador tributario de la tienda.
+ */
+
 public class TiendaComputadores {
+    private String nombreTienda;
+    private String propiedadTienda;
+    private int idTributarioTienda;
+
+    public TiendaComputadores(String nombreTienda, String propiedadTienda, int idTributarioTienda) {
+        this.nombreTienda = nombreTienda;
+        this.propiedadTienda = propiedadTienda;
+        this.idTributarioTienda = idTributarioTienda;
+    }
+
+    public String getNombreTienda() {
+        return nombreTienda;
+    }
+
+    public void setNombreTienda(String nombreTienda) {
+        this.nombreTienda = nombreTienda;
+    }
+
+    public String getPropiedadTienda() {
+        return propiedadTienda;
+    }
+
+    public void setPropiedadTienda(String propiedadTienda) {
+        this.propiedadTienda = propiedadTienda;
+    }
+
+    public int getIdTributarioTienda() {
+        return idTributarioTienda;
+    }
+
+    public void setIdTributarioTienda(int idTributarioTienda) {
+        this.idTributarioTienda = idTributarioTienda;
+    }
 }
