8f28083cbfc64d2f478061d81c72e9ccd822c357
Franco Armando <francoarmando1911@gmail.com>
2025-04-19 09:57:30 -0300

    Actualiz§acion getters y setters

--- src/main/java/EjercicioCursoUniversitario/Estudiante.java
+++ src/main/java/EjercicioCursoUniversitario/Estudiante.java
@@ -16,6 +16,46 @@ public class Estudiante {
         this.notaFinal = notaFinal;
     }
 
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumeroSemestre() {
+        return numeroSemestre;
+    }
+
+    public void setNumeroSemestre(int numeroSemestre) {
+        this.numeroSemestre = numeroSemestre;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+
     /*
     ○ Añadir un estudiante al curso: se ingresan por teclado los datos
     del estudiante. El código del estudiante debe ser único, si el có-
