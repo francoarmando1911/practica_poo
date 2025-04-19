4f0f2e8546e7959d715272d5c06b573050c3d184
Franco Armando <francoarmando1911@gmail.com>
2025-04-19 09:56:44 -0300

    Constructor

---
+++ src/main/java/EjercicioCursoUniversitario/Curso.java
@@ -0,0 +1,14 @@
+package EjercicioCursoUniversitario;
+
+/*
+○ Calcular promedio del curso: sumar las notas de los estudiantes
+y dividirlas por la cantidad de estudiantes que tiene el curso.
+
+○ Obtener la cantidad de estudiantes que aprobó el curso: calcu-
+lar el número de estudiantes que obtuvo un promedio mayor o
+igual a 3.0 y mostrarlo en pantalla. También se debe calcular el
+porcentaje de estudiantes que aprobó el curso.
+ */
+public class Curso {
+
+}
--- src/main/java/EjercicioCursoUniversitario/Estudiante.java
+++ src/main/java/EjercicioCursoUniversitario/Estudiante.java
@@ -8,5 +8,26 @@ public class Estudiante {
     private int numeroSemestre;
     private double notaFinal;
 
+    public Estudiante(int idEstudiante, String nombre, String apellido, int numeroSemestre, double notaFinal) {
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroSemestre = numeroSemestre;
+        this.notaFinal = notaFinal;
+    }
 
+    /*
+    ○ Añadir un estudiante al curso: se ingresan por teclado los datos
+    del estudiante. El código del estudiante debe ser único, si el có-
+    digo ya existe se debe generar el mensaje correspondiente.
+
+    ○ Buscar un estudiante de acuerdo con su código ingresado por
+    teclado: si se encuentra muestra los datos del estudiante. De lo
+    contrario, debe mostrar el mensaje correspondiente.
+
+    ○ Eliminar un estudiante de acuerdo con su código ingresado por
+    teclado: si se encuentra muestra los datos del estudiante y se
+    solicita una confirmación de la eliminación. Si no, debe mostrar
+    el mensaje correspondiente.
+     */
 }
