17a537f8583ae1ea6000384cb018ba950963fac4
Franco Armando <francoarmando1911@gmail.com>
2025-04-19 09:54:09 -0300

    Actualizacion

--- src/main/java/EjercicioCursoUniversitario/Estudiante.java
+++ src/main/java/EjercicioCursoUniversitario/Estudiante.java
@@ -1,4 +1,12 @@
 package EjercicioCursoUniversitario;
 
 public class Estudiante {
+
+    private int idEstudiante;
+    private String nombre;
+    private String apellido;
+    private int numeroSemestre;
+    private double notaFinal;
+
+
 }
