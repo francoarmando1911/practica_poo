34869199f09940ab1e15cf23ee013d314ce4d55e
Franco Armando <francoarmando1911@gmail.com>
2025-04-20 22:35:49 -0300

    Actualizacion

---
+++ src/main/java/ejerciciosCande/ejercicioNumeros.java
@@ -0,0 +1,20 @@
+package ejerciciosCande;
+
+/*
+hace un programa que reciba un numero y te calcule el doble
+
+Tipos de datos:
+- int
+- String
+- double
+- boolean
+- char
+
+ */
+public class ejercicioNumeros {
+    public static void main(String[] args) {
+        int numero = 5;
+        int dobleNumero = (numero * 2);
+        System.out.println(dobleNumero);
+    }
+}
