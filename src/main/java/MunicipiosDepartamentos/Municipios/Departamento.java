5f6ab5136daf3cce32d301ae55ac536de863d9f3
Franco Armando <francoarmando1911@gmail.com>
2025-06-14 18:27:59 -0300

    Clases Municipio y Departamento de ejercicio DepartamentosMunicipios

--- src/main/java/MunicipiosDepartamentos/Municipios/Departamento.java
+++ src/main/java/MunicipiosDepartamentos/Municipios/Departamento.java
@@ -1,4 +1,76 @@
 package MunicipiosDepartamentos.Municipios;
 
+import java.util.List;
+
 public class Departamento {
+
+    private String nombre;
+    private List<Municipio> municipios;
+
+    public Departamento(String nombre, List<Municipio> municipios) {
+        this.nombre = nombre;
+        this.municipios = municipios;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Municipio> getMunicipios() {
+        return municipios;
+    }
+
+    public void setMunicipios(List<Municipio> municipios) {
+        this.municipios = municipios;
+    }
+
+    public void agregarMunicipio(Municipio municipio) {
+        this.municipios.add(municipio);
+    }
+
+    public void eliminarMunicipio(String nombre) {
+        Municipio municipio;
+        for (int i = 0; i < this.municipios.size(); i++) {
+            if (this.municipios.get(i).getNombre().equals(nombre)) {
+                this.municipios.remove(i);
+                break;
+            }
+        }
+    }
+
+    public void buscarMunicipio(String nombre) {
+        Municipio municipio;
+        for (int i = 0; i < this.municipios.size(); i++) {
+            if (this.municipios.get(i).getNombre().equals(nombre)) {
+                municipio = this.municipios.get(i);
+                break;
+            }
+            if(!nombre.equals(getNombre())){
+                System.out.println("Municipio no encontrado");
+            }
+        }
+    }
+
+    public void buscarMunicipioMayorPoblacion(Integer poblacion) {
+        Municipio municipio;
+        boolean encontroMunicipios = false;
+
+        for (int i = 0; i < this.municipios.size(); i++) {
+            municipio = this.municipios.get(i);
+
+            if (municipio.getPoblacion() >= poblacion) {
+                System.out.println(municipio.getNombre());
+                encontroMunicipios = true;
+            }
+        }
+        if (!encontroMunicipios) {
+            System.out.println("No existe municipios con esa poblacion");
+        }
+    }
+
+
 }
--- src/main/java/MunicipiosDepartamentos/Municipios/Municipio.java
+++ src/main/java/MunicipiosDepartamentos/Municipios/Municipio.java
@@ -1,4 +1,58 @@
 package MunicipiosDepartamentos.Municipios;
 
 public class Municipio {
+
+    private String nombre;
+    private Integer poblacion;
+    private Double temperaturaMedia;
+    private Double altitud;
+
+    public Municipio(String nombre, Integer poblacion, Double temperaturaMedia, Double altitud) {
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+        this.temperaturaMedia = temperaturaMedia;
+        this.altitud = altitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(Integer poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public Double getTemperaturaMedia() {
+        return temperaturaMedia;
+    }
+
+    public void setTemperaturaMedia(Double temperaturaMedia) {
+        this.temperaturaMedia = temperaturaMedia;
+    }
+
+    public Double getAltitud() {
+        return altitud;
+    }
+
+    public void setAltitud(Double altitud) {
+        this.altitud = altitud;
+    }
+
+    @Override
+    public String toString() {
+        return "Municipio{" +
+                "nombre='" + nombre + '\'' +
+                ", poblacion=" + poblacion +
+                ", temperaturaMedia=" + temperaturaMedia +
+                ", altitud=" + altitud +
+                '}';
+    }
 }
